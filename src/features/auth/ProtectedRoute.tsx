1577edf8f30e57da0560651abd764ba881a12c74
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-04-21 22:42:45 +0800

    Create ProtectedRoute.tsx

---
+++ src/features/auth/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+interface ProtectedRouteProps {
+  allowedRoles?: string[];
+  children?: React.ReactNode;
+}
+
+export const ProtectedRoute = ({ allowedRoles, children }: ProtectedRouteProps) => {
+  const { uid, role } = useAuth();
+  const token = localStorage.getItem('token');
+
+  // If No UID and No Token (Double check for hydration/cache)
+  if (!uid && !token) {
+    return <Navigate to="/" replace />;
+  }
+
+  // If specific roles are required
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    return <Navigate to="/app/dashboard" replace />;
+  }
+
+  return children ? <>{children}</> : <Outlet />;
+};
